2c84dc99d78a0732164af33ae4a5c4cbc7d158c8
corneliusventi <corneliusventi@gmail.com>
2025-02-28 01:54:38 +0700

    add LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -53,8 +53,31 @@ class TestHTMLNode(unittest.TestCase):
             }
         )
         self.assertEqual(
-            "HTMLNode(tag=button, value=Submit, props={'type': 'submit'}, children=None)", repr(button_node)
+            "HTMLNode(button, Submit, children=None, {'type': 'submit'})", repr(button_node)
         )
+
+    def test_leaf_to_html_p(self):
+        p_node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(
+            "<p>This is a paragraph of text.</p>", p_node.to_html()
+        )
+
+    def test_leaf_to_html_a(self):
+        a_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            '<a href="https://www.google.com">Click me!</a>', a_node.to_html()
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        text_node = LeafNode(None, "This is a text.")
+        self.assertEqual(
+            "This is a text.", text_node.to_html()
+        )
+
+    def test_leaf_to_html_no_value(self):
+        leaf_node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            leaf_node.to_html()
 
 
 if __name__ == "__main__":
