15e91a8bb9448b975f4025d27c1800ad2bfc186b
corneliusventi <corneliusventi@gmail.com>
2025-03-05 18:00:34 +0700

    add markdown_to_blocks

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,59 @@
+import unittest
+
+from markdown_blocks import (
+    markdown_to_blocks
+)
+
+
+class TestMarkdownBlocks(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_multiple(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+
+This is yet another **bolded** paragraph
+
+Another paragraph with _italic_ words and some `inline code`
+Still part of the same paragraph on a new line
+
+- Another list
+- with more items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+                "This is yet another **bolded** paragraph",
+                "Another paragraph with _italic_ words and some `inline code`\nStill part of the same paragraph on a new line",
+                "- Another list\n- with more items",
+            ],
+        )
+
