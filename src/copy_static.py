5a0034917a0bdfb93c6746cbfd620db9c127ec33
corneliusventi <corneliusventi@gmail.com>
2025-03-15 20:23:28 +0700

    Add static files and copy_static

---
+++ src/copy_static.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+
+def copy_static(to_path="./public", from_path="./static"):
+    if not os.path.exists(to_path):
+        os.mkdir(to_path)
+    items = os.listdir(from_path)
+    for item in items:
+        new_to_path = os.path.join(to_path, item)
+        new_from_path = os.path.join(from_path, item)
+        if os.path.isfile(new_from_path):
+            shutil.copy(new_from_path, new_to_path)
+        else:
+            copy_static(new_to_path, new_from_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from copy_static import copy_static
+
+
+public_path="./public"
+static_path="./static"
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    copy_static()
 
 
 if __name__ == "__main__":
